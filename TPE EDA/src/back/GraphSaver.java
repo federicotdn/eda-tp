e65b057c6621f7abd52006878854be7f9a0fd308
Hobbit <Hobbit@localhost>
2013-05-26 02:22:30 -0300

    toDot terminado

--- TPE EDA/src/back/GraphSaver.java
+++ TPE EDA/src/back/GraphSaver.java
@@ -15,15 +15,28 @@ public class GraphSaver {
 	FileWriter fileOutput = new FileWriter("grafo.dot");
 	BufferedWriter writer = new BufferedWriter(fileOutput);
 	
-	writer.write("digraph {");
+	writer.write("digraph prueba {");
 	
 	for(Node node: hGraph.nodes){
 	    writer.write(node.name + " [label=" + node.name + "]");
+	    writer.newLine();
+	    
 	}
 	
 	for(HyperEdge edge: hGraph.hEdges){
 	    writer.write(edge.name +  " [shape=box, height=0.18, fontsize=12, label=" + edge.name + "]");
 	    writer.newLine();
+	    
+	    for(Node node: edge.heads){
+		writer.write(node.name +  "->" + edge.name);
+		writer.newLine();
+	    }
+	    
+	    for(Node node: edge.tails){
+		writer.write(edge.name +  "->" + node.name);
+		writer.newLine();
+	    }
+	    
 
 	}
 	writer.write("}");
